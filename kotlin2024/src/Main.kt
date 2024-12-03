9cfc86a7371389ac2b2cc5f62ec6c007296152f6
duynghiadev <duynghia22302@gmail.com>
2024-12-03 11:18:23 +0700

    Bài 3: Biến và kiểu dữ liệu trong kotlin

--- kotlin2024/src/Main.kt
+++ kotlin2024/src/Main.kt
@@ -1,7 +1,37 @@
 fun main() {
-    // this is a comment
-    val name = "Kotlin"
+    // this is a comment -> one line
+
+    /*
+    * comment multiple lines
+    */
+
+    val name: String = "Kotlin" // this is a constant -> no change
+    var name1: String = "duynghia" // this is a variable -> can change
+
+//    name = "Ronaldo" // error
+    name1 = "Messi" // no error
+
     println("Hello, $name!")
+    println("Hello, $name1!")
+
+//    convenient variable name
+    var nameProduct: String = "computer"
+    println("nameProduct: $nameProduct")
+
+//    kiểu số nguyên
+    var b: Byte = 127
+    println("Kieu du lieu cua $b la: " + (b::class.java.typeName))
+
+//    khai báo mảng số nguyên
+    var arrayNumber: IntArray = intArrayOf(34, 21, 23, 14)
+    println("arrayNumber: " + arrayNumber.contentToString())
+
+//    khai báo mảng số thực
+    var arrayFloat: FloatArray = floatArrayOf(34.5F, 234.5F, 234.5F)
+    println("arrayFloat: " + arrayFloat.contentToString())
+
+//    khai báo mảng string
+    println("Phần tử thứ nhất trong arrayNumber là ${arrayNumber[0]}")
 
     for (i in 1..5) {
         println("i = $i")
