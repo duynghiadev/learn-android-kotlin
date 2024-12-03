808fc46387ab9c2b3759a1ddab68652ed4955878
duynghiadev <duynghia22302@gmail.com>
2024-12-03 10:10:35 +0700

    Kotlin 004 - Tìm số Fibonacci Fn

---
+++ duynghiakotlin/src/video05.kt
@@ -0,0 +1,26 @@
+fun main() {
+    println("Vui lòng nhập số nguyên dương 0-90: ")
+    var n: Int = readln().toInt()
+    if (n >= 0 && n <= 90) {
+        var fn = fibonacci(n)
+        println("fn: $fn")
+    } else {
+        println("Vui lòng nhập n trong đoạn từ 0 đến 90")
+    }
+}
+
+fun fibonacci(n: Int): Long {
+    if (n < 2) {
+        return n.toLong()
+    }
+    var f0: Long = 0
+    var f1: Long = 1
+    var fn: Long = 0
+
+    for (i in 2..n) {
+        fn = f0 + f1
+        f0 = f1
+        f1 = fn
+    }
+    return fn
+}
