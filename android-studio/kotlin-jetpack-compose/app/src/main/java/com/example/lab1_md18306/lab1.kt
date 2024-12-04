f417e9dd15212b251e643a958034d02a02df6b4b
duynghiadev <duynghia22302@gmail.com>
2024-12-04 12:04:13 +0700

    format project structure

---
+++ android-studio/kotlin-jetpack-compose/app/src/main/java/com/example/lab1_md18306/lab1.kt
@@ -0,0 +1,9 @@
+package com.example.lab1_md18306
+
+fun main() {
+    println("Hello Kotlin - bai 1")
+    println("---------------------------")
+}
+
+class lab1 {
+}
--- kotlin2024/README.md
+++
@@ -1,1 +0,0 @@
-[Link video](https://www.youtube.com/playlist?list=PLGQ_L_1sA-DPg3UwwdYMJQndcNgq31nc-)
