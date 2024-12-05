b918af119b4586e7ae3919f93a345eb0b2df8e6e
duynghiadev <duynghia22302@gmail.com>
2024-12-05 10:47:38 +0700

    KOT104 - Chữa Lab 7 - Sử dụng MVVM trong app Android Jetpack Compose

---
+++ android-studio/Lab7-KOT104/app/src/main/java/com/duyle/lab6_kot104_md18306/MovieViewModel.kt
@@ -0,0 +1,17 @@
+package com.duyle.lab6_kot104_md18306
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MovieViewModel : ViewModel() {
+
+    private val _movies = MutableLiveData<List<Movie>>()
+
+    val movies: LiveData<List<Movie>> = _movies
+
+    init {
+        _movies.value = Movie.getSampleMovies()
+    }
+
+}
