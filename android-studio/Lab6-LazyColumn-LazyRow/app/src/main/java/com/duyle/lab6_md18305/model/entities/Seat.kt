5fb3b3597f4efc60724d69276d5b63855aa691ba
duynghiadev <duynghia22302@gmail.com>
2024-12-05 10:10:16 +0700

    KOT104 - Chữa Lab 6 - Lập trình Android với Kotlin Jetpack Compose - LazyColumn, LazyRow, LazyGrid

---
+++ android-studio/Lab6-LazyColumn-LazyRow/app/src/main/java/com/duyle/lab6_md18305/model/entities/Seat.kt
@@ -0,0 +1,4 @@
+package com.duyle.lab6_md18305.model.entities
+
+data class Seat(var row: Char, val number: Int, var status:
+SeatStatus)
---
+++ android-studio/Lab6-LazyColumn-LazyRow/app/src/main/java/com/duyle/lab6_md18305/model/entities/SeatStatus.kt
@@ -0,0 +1,3 @@
+package com.duyle.lab6_md18305.model.entities
+
+enum class SeatStatus { EMPTY, SELECTED, BOOKED, AISLE }
